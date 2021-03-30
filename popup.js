84011abb06697c1baf097179cce25409177bd3e5
Nadav Goldstein <nadav.goldstein96@gmail.com>
2021-03-30 14:55:01 +0300

    added popup layout including basic logic

---
+++ popup.js
@@ -0,0 +1,26 @@
+//function getRandomColor() {
+//	var letters = '0123456789ABCDEF';
+//	var color = '#';
+//	for (var i = 0; i < 6; i++) {
+//	  color += letters[Math.floor(Math.random() * 16)];
+//	}
+//	return color;
+//  }
+  
+////  let changeColor = document.getElementById("changeColor");
+////  changeColor.style.backgroundColor = getRandomColor();
+//console.log("hello", $("#changeColor"));
+//$("#changeColor").text = "hello";
+//$("#changeColor").click(function() {
+//	let changeColor = document.getElementById("changeColor");
+//	changeColor.style.backgroundColor = getRandomColor();
+//})
+//// Initialize button with user's preferred color
+
+(() => {
+	'use strict';
+	$("#changeColor").click(function() {
+		//$("#changeColor").text("!!!");
+	});
+})();
+
