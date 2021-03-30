38894c04bd980424b303df346d83539a0ba58d79
Nadav Goldstein <nadav.goldstein96@gmail.com>
2021-03-30 15:15:58 +0300

    yes! storage is working

--- popup.js
+++ popup.js
@@ -1,22 +1,11 @@
 (() => {
 	'use strict';
 	chrome.storage.sync.get(['filter'], function(result) {
-		console.log("hello!!!!");
-		if (isNaN(result)) {
-			$("#current-filter").text("Helo");
-		}
-		else {
-			$("#current-filter").text(result.key);
-		}
+		$("#current-filter").text("current:" + result["filter"]);
 		$("#update-filter").click(function() {
-			//$("#update-filter").text("!!!");
 			var value = $("#new-filter").val();
-			var obj = {
-				"filter": value
-			}
-			chrome.storage.sync.set(obj, function() {
-				$("#current-filter").text(value);
-				console.log('Value is set to ' + (value));
+			chrome.storage.sync.set({"filter": value}, function() {
+				$("#current-filter").text("current:" + value);
 			});
 		});
 	});
