87e64fcbdf7a3ba9fdef11aabb93782a77b04a00
Nadav Goldstein <nadav.goldstein96@gmail.com>
2021-03-30 22:24:23 +0300

    finished version 1 of trimoodle

--- content.js
+++ content.js
@@ -1,19 +1,16 @@
-console.log("hello!!!!!!", $("body"));
-$("#page-site-index").css("background-color", "yellow");
-$("li.type_course > p").each(function(i) {
-	var key = $(this).attr("data-node-key");
-	if (key == "80517") {
-		$(this).remove();
+chrome.storage.sync.get(['filter'], function(result) {
+	console.log('Value currently is ' + result["filter"]);
+	var value = result["filter"];
+	courses = value.split(",");
+	map = {};
+	for (var i=0; i<courses.length; i++) {
+		map[courses[i]] = true;
 	}
-})
 
-chrome.storage.sync.get(['key'], function(result) {
-	console.log('Value currently is ' + result.key);
-	var value = result.key;
-	if (isNaN(value)) {
-		value = 0;
-	}
-	chrome.storage.sync.set({"key": value+1}, function() {
-		console.log('Value is set to ' + (value + 1));
-	  });
+	$("li.type_course > p").each(function(i) {
+		var key = $(this).attr("data-node-key");
+		if (key in map) {
+			$(this).remove();
+		}
+	})
 });
--- popup.js
+++ popup.js
@@ -2,8 +2,9 @@
 	'use strict';
 	chrome.storage.sync.get(['filter'], function(result) {
 		$("#current-filter").text("current:" + result["filter"]);
+		$("#new-filter").val(result["filter"]);
 		$("#update-filter").click(function() {
-			var value = $("#new-filter").val();
+			var value = $("#new-filter").val().replace(" ", "");
 			chrome.storage.sync.set({"filter": value}, function() {
 				$("#current-filter").text("current:" + value);
 			});
