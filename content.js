92fe82aca6030f7e360e07fd344cbed3ce16fbb4
Nadav Goldstein <nadav.goldstein96@gmail.com>
2021-04-04 13:39:20 +0300

    added dynamic unhide button to the content

--- content.js
+++ content.js
@@ -12,65 +12,81 @@ function updateCoursesSidebar() {
 		}
 	
 		$("li.type_course > p").each(function(i) {
+			var parent = $(this).parent();
+
 			var key = $(this).attr("data-node-key");
 			if (key in map) {
-				$(this).remove();
+				parent.hide();
 			}
-			else {
-				// apply
-				var parent = $(this).parent();
-				parent.css("border-style", "solid")
-								.css("border-width", "1px")
-								.css("border-color", "#0A467E")
-								.css("margin-top","5px");
-				var button = $("<button class=\"fa fa-eye-slash\"></button>");
-				button.click(function() {
-					chrome.storage.sync.get(['filter'], function(result) {
-						var oldValue = result["filter"];
-						Snackbar.show({
-							pos: 'bottom-center', 
-							text: `Removed course ${key}`, 
-							actionText: 'Undo',
-							onActionClick: function(element) {
-								chrome.storage.sync.set({"filter": oldValue});
-								parent.show();
-								$(element).css('opacity', 0);
-							}});
-						var value = `${oldValue},${key}`;	
-						
-						chrome.storage.sync.set({"filter": value}, function() {
-							parent.hide();
-						});
+
+			// apply
+			parent.css("border-style", "solid")
+							.css("border-width", "1px")
+							.css("border-color", "#0A467E")
+							.css("margin-top","5px");
+
+			var button = $("<button class=\"fa fa-eye-slash\"></button>");
+			button.css("color", "#0A467E").css("float", isRtl ? "left" : "right")
+			.css("border", "none")
+			.css("text-decoration", "none")
+			.css("background-color", "transparent")
+			.css("font-size", "18px");
+			button.click(function() {
+				chrome.storage.sync.get(['filter'], function(result) {
+					var oldValue = result["filter"];
+					Snackbar.show({
+						pos: 'bottom-center', 
+						text: `Removed course ${key}`, 
+						actionText: 'Undo',
+						onActionClick: function(element) {
+							chrome.storage.sync.set({"filter": oldValue});
+							parent.show();
+							$(element).css('opacity', 0);
+						}});
+					var value = `${oldValue},${key}`;	
+					
+					chrome.storage.sync.set({"filter": value}, function() {
+						parent.hide();
 					});
 				});
-				button.css("color", "#0A467E").css("float", isRtl ? "left" : "right")
-										.css("border", "none")
-										.css("text-decoration", "none")
-										.css("background-color", "transparent")
-										.css("font-size", "18px");
-				$(this).before(button);
-			}
+			});
+			$(this).before(button);
 		});
 	});
 }
 
 var isRtl = $("html").attr("dir") == "rtl";
 
-chrome.storage.sync.get(['filter'], function(result) {
-	console.log('Value currently is ' + result["filter"]);
-
-	// add the reset button
-	var resetText = isRtl ? "הצג את כל הקורסים" : "unhide all courses";
-	var resetButton = $(`<button>${resetText}</button>`);
-	resetButton.click(function() {
-		chrome.storage.sync.set({"filter": ""});
-		location.reload();
-	});
-	resetButton.css(isRtl ? "margin-left" : "margin-right", "10px")
-				.css("color", "#0A467E")
-				.css("border", "none")
-				.css("text-decoration", "none")
-				.css("background-color", "transparent")
-	$(".header-actions-container").append(resetButton)
-	updateCoursesSidebar();
+var root = $("li.type_system > p").first();
+root.click(false);
+var button = $("<button class=\"fa fa-eye\"></button>");
+button
+	.css("color", "#0A467E")
+	.css("float", isRtl ? "left" : "right")
+	.css("border", "none")
+	.css("text-decoration", "none")
+	.css("background-color", "transparent")
+	.css("font-size", "18px");
+button.css("color", "#D98901")
+button.click(function() {
+	chrome.storage.sync.set({"filter": ""});
+	$("li.type_course").each(function(i) {
+		$(this).show();
+	})
 });
+root.append(button);
+
+// add the reset button
+var resetText = isRtl ? "הצג את כל הקורסים" : "unhide all courses";
+var resetButton = $(`<button>${resetText}</button>`);
+resetButton.click(function() {
+	chrome.storage.sync.set({"filter": ""});
+	location.reload();
+});
+resetButton.css(isRtl ? "margin-left" : "margin-right", "10px")
+			.css("color", "#0A467E")
+			.css("border", "none")
+			.css("text-decoration", "none")
+			.css("background-color", "transparent")
+$(".header-actions-container").append(resetButton)
+updateCoursesSidebar();
