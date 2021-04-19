2fa5115811101e02f53c2522f5ba78ed0f09ea78
Nadav Goldstein <nadav.goldstein96@gmail.com>
2021-04-20 00:17:00 +0300

    added rating dialog (gracfully dismiss it if the user don't want it)

--- content.js
+++ content.js
@@ -15,7 +15,6 @@ function updateUnhideButtonStatus() {
 	});
 }
 
-
 function updateCoursesSidebar() {
 	chrome.storage.sync.get(['filter'], function(result) {
 		var value = result["filter"];
@@ -65,7 +64,7 @@ function updateCoursesSidebar() {
 						}});
 					var value = `${oldValue},${key}`;	
 					
-					chrome.storage.sync.set({"filter": value}, function() {
+					chrome.storage.sync.set({"filter": value, "actionComplete": true}, function() {
 						updateUnhideButtonStatus();
 						parent.hide();
 					});
@@ -73,6 +72,61 @@ function updateCoursesSidebar() {
 			});
 			$(this).before(button);
 		});
+	});
+}
+
+function addRateDialog() {
+	var isHomepage = $("ol.breadcrumb li").length <= 2;
+	if (!isHomepage) {
+		return;
+	}
+
+	chrome.storage.sync.get(['actionComplete', 'rateDialogDismiss'], function(result) {
+		if (result["rateDialogDismiss"] == true) {
+			return;
+		}
+		if (result["actionComplete"] == true) {
+			var container = $("<div></div>");
+			container.css("background-color", "#F0FAFF")
+					.css("margin-left", "13px")
+					.css("margin-right", "13px")
+					.css("border-style", "solid")
+					.css("border-width", "1px");
+		
+			var title = $("<p>Enjoying Trimoodle so far?</p>")
+			title.css("text-align", "center");
+			title.css("font-size", "large");
+			title.css("font-weight", "bold");
+			title.css("padding-top", "15px");
+		
+			var info = $("<p>If so, please consider giving us a 5 star rating :)</p>")
+			info.css("text-align", "center");
+		
+			var actions = $("<div></div>");
+			actions.attr("align", "center");
+			actions.css("padding-bottom", "15px");
+			var ok = $("<button>Rate me!</button>");
+			ok.css("margin-right", "10px");
+			var dismiss = $("<button>No thank you</button>");
+		
+			ok.click(function() {
+				window.open("https://chrome.google.com/webstore/detail/trimoodle/najiiglmdjablddfmgegpookgecbddej/reviews")
+				container.remove();
+				chrome.storage.sync.set({"rateDialogDismiss": true});
+			});
+			dismiss.click(function() {
+				container.remove();
+				chrome.storage.sync.set({"rateDialogDismiss": true});
+			});
+		
+			container.append(title);
+			container.append(info);
+			actions.append(ok);
+			actions.append(dismiss);
+			container.append(actions);
+		
+			$("#region-main").prepend(container);
+		}
 	});
 }
 
@@ -100,3 +154,4 @@ root.append(button);
 
 updateUnhideButtonStatus();
 updateCoursesSidebar();
+addRateDialog();
